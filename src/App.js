8eebb73f9be3c12a3e50131590842f2b81210977
Jason Pembleton CW <A6CH1ZZ@usac.mmm.com>
2017-06-21 16:32:12 -0400

    learning

--- src/App.js
+++ src/App.js
@@ -17,34 +17,98 @@ const list = [{
 	objectID: 1,
 }];
 
+const isSearched = (searchTerm) => (item) => 
+	!searchTerm || item.title.toLowerCase().includes(searchTerm.toLowerCase()); 
+	// function returns a function that checks to see if the list title is included in the user search query
+	//includes() https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Array/includes?v=control
+
 class App extends Component {
 	constructor(props) {
 		super(props);
 
 		this.state = {
 			list,
+			searchTerm: ''
 		};
+
+		this.onDismiss = this.onDismiss.bind(this);
+		this.onSearchChange = this.onSearchChange.bind(this);
+	}
+
+	onDismiss(id) {		
+		const isNotId = item => item.objectID !== id;
+		const updatedList = this.state.list.filter(isNotId);
+		//filter() https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Array/filter?v=control	
+		this.setState({list: updatedList});	
+	}
+
+	onSearchChange(event) {
+		this.setState({searchTerm: event.target.value});
 	}
 
 	render() {
+		const { list,searchTerm } = this.state; //destructuring
 		return (
-			<div className="App">
-				{this.state.list.map(item =>					
-					<div key={item.objectID}>
-						<span>
-							<a href={item.url}>{item.title}</a>
-						</span>
-						<span>{item.author}</span>
-						<span>{item.num_comments}</span>
-						<span>{item.points}</span>
-						<span>
-						
-						</span>
-					</div>				
-				)}
+			<div className="page">
+				<div className="interactions">
+					<Search 
+						value={searchTerm}
+						onChange={this.onSearchChange}
+					>
+						Search 
+					</Search>	
+					<Table 
+						list={list}
+						pattern={searchTerm}
+						onDismiss={this.onDismiss}
+					/>
+				</div>
 			</div>
 		);
 	}
 }
+
+const Search = ({value, onChange, children}) =>	//deconstruct the props here
+	<form>
+		{children}
+		<input 
+			type="text"
+			value={value} //control the input. (make the value = to the state) let the state remain the single source of truth
+			onChange={onChange}
+		/>
+	</form>
+
+
+const Table = ({list, pattern, onDismiss}) =>	//deconstruct the props here	
+	<div className="table">
+		{list.filter(isSearched(pattern)).map(item =>					
+			<div key={item.objectID} className="table-row">
+				<span>
+					<a href={item.url}>{item.title}</a>
+				</span>
+				<span>{item.author}</span>
+				<span>{item.num_comments}</span>
+				<span>{item.points}</span>
+				<span>
+					<Button 
+						onClick={() => onDismiss(item.objectID)}
+						className="button-inline"
+					>
+					Dismiss
+					</Button>
+				</span>
+			</div>				
+		)}
+	</div>
+
+const Button = ({onClick, className = "", children}) =>	//deconstruct the props here		
+	<button 
+		onClick={onClick}
+		className={className}
+		type="button"
+	>
+		{children}
+	</button>
+
 
 export default App;
